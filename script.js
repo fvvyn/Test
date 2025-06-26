06f9495cfc7a539161c234448aa529c3144f0818
fvvyn <fvvyn.brand@gmail.com>
2025-06-26 22:54:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+// ✅ 背景色を白に設定！
+renderer.setClearColor(0xffffff); 
+
 document.body.appendChild(renderer.domElement);
+
 
 // 環境光
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
